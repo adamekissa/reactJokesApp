e4f8bae020945ced001be32d14c5d3b80ca6f4cd
Mohammed Adam <adamekissa@gmail.com>
2024-06-13 13:43:22 +0100

    t

--- src/jokeList.jsx
+++
@@ -1,6 +0,0 @@
-const jokeList = [{title:"Star wars Theme" , joke: "How many storm troopers does it take to change a lightbulb? None, because they are all on the dark side."},
-    {title: "Meditation", joke: "What do you call a dog who meditates? Aware wolf."},
-    {title: "Breakfast theme", joke: "Why did the egg have a day off? Because it was Fryday."},
-    {title: "Grape wine", joke: "What did the grape say when it got stepped on? Nothing, just a little wine."}
-];
- export jokeList
